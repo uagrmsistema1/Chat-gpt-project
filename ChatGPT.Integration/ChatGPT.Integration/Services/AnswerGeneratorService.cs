eefbbf31ed5236dbcc7af7fc278ace57f88e9013
uagrmsistema1 <ronald.ardaya@gmail.com>
2023-03-28 20:48:58 -0400

    first commit

---
+++ ChatGPT.Integration/ChatGPT.Integration/Services/AnswerGeneratorService.cs
@@ -0,0 +1,29 @@
+﻿using OpenAI_API;
+using OpenAI_API.Completions;
+
+namespace ChatGPT.Integration.Services
+{
+    public class AnswerGeneratorService : IAnswerGeneratorService
+    {
+        public async Task<string> GenerateAnswer(string prompt)
+        {
+            string apikey = "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";
+            string answer = string.Empty;
+            var openai = new OpenAIAPI(apikey);
+            CompletionRequest completion= new CompletionRequest();
+            completion.Prompt= prompt;
+            completion.MaxTokens = 4000;
+            var result= await openai.Completions.CreateCompletionAsync(completion);
+            if (result!=null)
+            {
+                foreach (var item in result.Completions)
+                {
+                    answer = item.Text;
+                }
+            }
+            return answer;
+
+
+        }
+    }
+}
---
+++ ChatGPT.Integration/ChatGPT.Integration/Services/IAnswerGeneratorService.cs
@@ -0,0 +1,7 @@
+﻿namespace ChatGPT.Integration.Services
+{
+    public interface IAnswerGeneratorService
+    {
+        Task<string> GenerateAnswer(string prompt);
+    }
+}
